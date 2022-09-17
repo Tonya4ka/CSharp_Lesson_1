53372e15058eb5cd6a9f3c2f6ce3cffbbd9c951e
tonyatrushkova <antonina.trushkova@gmail.com>
2022-09-18 01:34:36 +0300

    Add Ex_2

---
+++ Ex_2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Type a number ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Type a number ");
+int number2 = int.Parse(Console.ReadLine());
+if (number1 > number2)
+{
+Console.WriteLine($"{number2} is min and {number1} is max");
+}
+else
+{
+Console.WriteLine($"{number1} is min and {number2} is max");
+}
