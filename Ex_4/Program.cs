7ac265bce51d197ddeaf86f4ad4f3a7ba8b49fd5
tonyatrushkova <antonina.trushkova@gmail.com>
2022-09-18 02:44:45 +0300

    Add Ex_4

---
+++ Ex_4/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+Console.WriteLine("Type a number ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Type a number ");
+int number2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Type a number ");
+int number3 = int.Parse(Console.ReadLine());
+
+if ((number1 > number2) & (number1 > number3))
+{
+Console.WriteLine($"{number1} is max");
+}
+else if ((number1 > number2) & (number1 < number3))
+{
+Console.WriteLine($"{number3} is max");
+}
+else if ((number1 < number2) & (number2 > number3))
+{
+Console.WriteLine($"{number2} is max");
+}
+else if ((number1 < number2) & (number2 < number3))
+{
+Console.WriteLine($"{number3} is max");
+}
