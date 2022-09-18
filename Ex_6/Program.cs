97889711226dc4f56d8976450619531cb6fd02ce
tonyatrushkova <antonina.trushkova@gmail.com>
2022-09-18 13:41:32 +0300

    Add Ex_6

---
+++ Ex_6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Type a number ");
+int number = int.Parse(Console.ReadLine());
+if (number%2==0)
+{
+Console.WriteLine($"{number} is even");
+}
+else
+{
+Console.WriteLine($"{number} is odd");
+}
