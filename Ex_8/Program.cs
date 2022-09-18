d4d4fc2141d45009e2d7188db6c64c7cf5dcd100
tonyatrushkova <antonina.trushkova@gmail.com>
2022-09-18 14:14:21 +0300

    Add Ex_8

---
+++ Ex_8/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Type a number ");
+int number = int.Parse(Console.ReadLine());
+
+if (number > 0)
+{
+    for (int i=1; i <= number; i++)
+    {
+        if (i%2==0)
+        {
+            Console.Write($"{i} ");
+        }
+    }
+}
+else
+{
+    Console.WriteLine("Not accepting negative numbers");
+}
